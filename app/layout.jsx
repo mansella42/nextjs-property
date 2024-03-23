822f9f6d8b4ae7405712a8feee6ada3b62f8731f
Alex Mansell <aualex42@gmail.com>
2024-03-23 09:52:15 +0000

    init repo

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import '@/assets/styles/globals.css';
+
+export const metadata = {
+    title: 'PropertyPulse | Find the perfect property',
+    description: 'Find the home for you',
+    keywords: 'rentals, purchases, home buying, real estate',
+};
+
+const mainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+
+export default mainLayout;
