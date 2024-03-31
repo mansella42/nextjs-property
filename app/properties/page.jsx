ca52d0ec404a6de5409f41d6b39d9896a7a16218
Alex Mansell <aualex42@gmail.com>
2024-03-31 15:30:36 +0100

    starting property card

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-import Link from 'next/link';
+import Link from "next/link";
+import PropertyCard from "@/components/PropertyCard";
+import properties from "@/properties.json";
 
 const propertiesPage = () => {
-  return <div>
-      <h1 className="text-5xl">Properties</h1>
-      <Link href="/">Go home</Link>
-  </div>;
+  return (
+    <section class="px-4 py-6">
+      <div class="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default propertiesPage;
